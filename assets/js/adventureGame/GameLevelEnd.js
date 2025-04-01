df3cb659b9159b078541f2318e71904053c9c039
Ruhaan Bansal <ruhamobile@gmail.com>
2025-04-01 13:31:43 -0700

    im see if it layers over like gameleveltest

--- assets/js/adventureGame/GameLevelEnd.js
+++ assets/js/adventureGame/GameLevelEnd.js
@@ -2,6 +2,7 @@ import GamEnvBackground from './GameEnvBackground.js';
 import Player from './Player.js';
 import Npc from './Npc.js';
 import Quiz from './Quiz.js';
+import BackgroundParallax from './BackgroundParallax.js';
 import GameControl from './GameControl.js';
 
 class GameLevelEnd {
@@ -84,6 +85,7 @@ class GameLevelEnd {
 
     this.classes = [
       { class: GamEnvBackground, data: image_data_end },
+            { class: BackgroundParallax, data: {src:  path + "/images/platformer/backgrounds/snowfall.png", zIndex: 2 } },
       { class: Player, data: sprite_data_chillguy },
       { class: Npc, data: sprite_data_tux }
     ];
