846f7b55cd588adb2fdc75d964aa789410512ad5
Ruhaan Bansal <ruhamobile@gmail.com>
2025-04-01 07:27:00 -0700

    game js change order of levels

--- assets/js/adventureGame/Game.js
+++ assets/js/adventureGame/Game.js
@@ -30,13 +30,13 @@ class Game {
         
 
         // start the game
-        const gameLevelClasses = [GameLevelDesert, GameLevelEnd, GameLevelSquares]
+        const gameLevelClasses = [GameLevelEnd, GameLevelDesert, GameLevelSquares]
         new GameControl(this, gameLevelClasses).start();
 
         // Show instructions before starting the game
         this.showInstructions(() => {
             // start the game after instructions are closed
-            const gameLevelClasses = [GameLevelDesert, GameLevelSquares]
+            const gameLevelClasses = [GameLevelEnd, GameLevelDesert, GameLevelSquares]
             new GameControl(this, gameLevelClasses).start();
         });
     }
