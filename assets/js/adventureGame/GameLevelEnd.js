39ad0bfeac5aa7a6b324d811910b9600dba4f55f
EvanSvetina <evanthehedgehog43@gmail.com>
2025-04-03 14:12:27 -0700

    tux schizophrenic breakdown

--- assets/js/adventureGame/GameLevelEnd.js
+++ assets/js/adventureGame/GameLevelEnd.js
@@ -45,13 +45,13 @@ class GameLevelEnd {
     };
 
     const sprite_src_tux = path + "/images/gamify/tux.png";
-    const sprite_greet_tux = "Hi, I am Tux, the Linux mascot. I am very happy to spend some Linux shell time with you!";
+    const sprite_greet_tux = "This is the end...";
     const sprite_data_tux = {
         id: 'Tux',
         greeting: sprite_greet_tux,
         src: sprite_src_tux,
         SCALE_FACTOR: 8,
-        ANIMATION_RATE: 50,
+        ANIMATION_RATE: 1000000,
         pixels: {height: 256, width: 352},
         INIT_POSITION: { x: (width / 2), y: (height / 2) },
         orientation: {rows: 8, columns: 11 },
@@ -60,16 +60,7 @@ class GameLevelEnd {
         quiz: { 
           title: "Linux Command Quiz",
           questions: [
-            "Which command is used to list files in a directory?\n1. ls\n2. dir\n3. list\n4. show",
-            "Which command is used to change directories?\n1. cd\n2. chdir\n3. changedir\n4. changedirectory",
-            "Which command is used to create a new directory?\n1. mkdir\n2. newdir\n3. createdir\n4. makedir",
-            "Which command is used to remove a file?\n1. rm\n2. remove\n3. delete\n4. erase",
-            "Which command is used to remove a directory?\n1. rmdir\n2. removedir\n3. deletedir\n4. erasedir",
-            "Which command is used to copy files?\n1. cp\n2. copy\n3. duplicate\n4. xerox",
-            "Which command is used to move files?\n1. mv\n2. move\n3. transfer\n4. relocate",
-            "Which command is used to view a file?\n1. cat\n2. view\n3. show\n4. display",
-            "Which command is used to search for text in a file?\n1. grep\n2. search\n3. find\n4. locate",
-            "Which command is used to view the contents of a file?\n1. less\n2. more\n3. view\n4. cat" 
+            "It's eternity in here! It's eternity in here! It's eternity in here! It's eternity in here! It's eternity in here! It's eternity in here! It's eternity in here! It's eternity in here! \n1. huh\n2. what\n3. ...\n4. ok bye"
           ] 
         },
         reaction: function() {
